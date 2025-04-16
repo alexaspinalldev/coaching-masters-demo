c97a7fe495dd7a07aaa6f3afac914c176228a1c3
Alex Aspinall <alex.aspinall@blueyonder.co.uk>
2025-04-16 12:37:14 +0100

    Feat(Lesson): Fix issue with parsing of youtube URL to allow for embedding

--- cmdemo/src/app/components/moduleView.tsx
+++ cmdemo/src/app/components/moduleView.tsx
@@ -3,39 +3,41 @@
 import mockData from "@/app/data/mockData.json";
 import React, { useState } from "react";
 
+interface Lesson {
+    lessonId: number;
+    title: string;
+    videoSrc: string;
+}
+interface Module {
+    moduleId: number;
+    title: string;
+    lessons: Lesson[];
+}
+// We'll infer these from supabase schema later
+
+const courseModules: Module[] = mockData;
+
 export default function ModuleView({ moduleId }: { moduleId: number }) {
-    interface Lesson {
-        lessonId: number;
-        title: string;
-        videoSrc: string;
-    }
-
-    interface Module {
-        moduleId: number;
-        title: string;
-        lessons: Lesson[];
-    }
-
-    const courseModules: Module[] = mockData;
-
-    const [selectedLesson, setSelectedLesson] = useState<Lesson | null>(null);
 
     const module = courseModules.find(
         (module) => module.moduleId === Number(moduleId)
     );
 
+    const [selectedLesson, setSelectedLesson] = useState<Lesson | null>(module?.lessons[0] || null);
+
+    // Sanitise the youtube URL to allow the use of normal browser URL in the database
+    let videoId = selectedLesson?.videoSrc.split("v=")[1];
+    if (videoId?.includes("&")) {
+        videoId = videoId.split("&")[0];
+    }
+    const embedUrl = `https://www.youtube.com/embed/${videoId}`
+
     return (
         <div>
-            <h1>{module?.title || "Module not found"} </h1>
+            <h1>{module!.title}</h1>
             {
                 selectedLesson?.videoSrc && (
-                    <iframe
-                        src={selectedLesson.videoSrc}
-                        width="640"
-                        height="360"
-                        allowFullScreen
-                        title={selectedLesson.title}
-                    > </iframe>
+                    <iframe className="w-full" src={embedUrl} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
                 )
             }
             <ol>
--- cmdemo/src/app/page.tsx
+++ cmdemo/src/app/page.tsx
@@ -2,7 +2,8 @@ export default function Home() {
   return (
     <div>
       <main>
-        <h1>Homepage</h1>
+        <h1>Welcome to The Coching Masters</h1>
+        <a href="/modules">Get started by exploring our modules.</a>
       </main>
     </div >
   );
