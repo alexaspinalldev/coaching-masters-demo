480924ca8e79ca321b0cc441f134948d7b68c744
Alex Aspinall <alex.aspinall@blueyonder.co.uk>
2025-04-16 11:02:55 +0100

    Feat(Module)Add mapping of lessons within module page

--- cmdemo/src/app/modules/[moduleId]/page.tsx
+++ cmdemo/src/app/modules/[moduleId]/page.tsx
@@ -1,9 +1,22 @@
+import mockData from "@/app/data/mockData.json";
+const courseModules = mockData;
+
 export default async function Module({ params }: Readonly<{ params: Promise<{ moduleId: string }> }>) {
   const { moduleId } = await params;
+  const module = courseModules.find((module: { moduleId: number }) => module.moduleId === Number(moduleId));
   return (
     <div>
       <main>
-        <h1>{moduleId}</h1>
+
+        <h1>{module?.title}</h1>
+        <ol>
+          {module?.lessons.map((lesson: { lessonId: number, title: string }) => (
+            <li key={lesson.lessonId}>
+              <div>{lesson.title}</div>
+              <input type="checkbox" />
+            </li>
+          ))}
+        </ol>
       </main>
     </div >
   );
