029542b4e64068349eb5cfd9418d47f453c4a916
Alex Aspinall <alex.aspinall@blueyonder.co.uk>
2025-04-18 16:58:12 +0100

    Fix (Theme): Resolve hydration issue by conditionally rendering the component more precisely

--- cmdemo/src/app/components/ui/themeToggle.tsx
+++ cmdemo/src/app/components/ui/themeToggle.tsx
@@ -1,22 +1,30 @@
 "use client"
 
 import * as React from "react"
+import { useEffect, useState } from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 import { Button } from "@/app/components/ui/button"
 
 export function ThemeToggle() {
-    const { theme, setTheme } = useTheme()
-    const toggleTheme = () => {
-        setTheme(theme === "dark" ? "light" : "dark")
-    }
+    const { setTheme, theme, resolvedTheme } = useTheme()
+    const [mounted, setMounted] = useState(false)
+
+    // Confirm if the component is now mounted
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    const isDark = resolvedTheme === 'dark'
 
     return (
-        <Button variant="outline" size="icon" onClick={toggleTheme}>
-            {!theme ? null : theme === "dark" ? (
-                <Sun className="absolute h-[1.2rem] w-[1.2rem]" />
-            ) : (
-                <Moon className="absolute h-[1.2rem] w-[1.2rem]" />)}
+        <Button variant="outline" size="icon" onClick={() => setTheme(isDark ? 'light' : 'dark')}>
+            {!mounted ? null :
+                (!theme ? null : theme === "dark" ? (
+                    <Sun className="absolute h-[1.2rem] w-[1.2rem]" />
+                ) : (
+                    <Moon className="absolute h-[1.2rem] w-[1.2rem]" />))
+            }
 
             <span className="sr-only">Toggle theme</span>
         </Button >)
